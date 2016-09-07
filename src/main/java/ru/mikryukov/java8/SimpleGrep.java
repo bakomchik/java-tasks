b1daf5f1fc507bdd48ba52f76418a486bd817702
Mikryukov Evgeniy <mikryukov.evgeniy@gmail.com>
2016-09-07 17:07:31 +0300

    Fix for SimpleGrep

--- src/main/java/ru/mikryukov/java8/SimpleGrep.java
+++ src/main/java/ru/mikryukov/java8/SimpleGrep.java
@@ -3,11 +3,7 @@ package ru.mikryukov.java8;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
-
 
 /**
  * Created by evgeniy.mikryukov on 06.09.16.
@@ -15,29 +11,27 @@ import java.util.stream.Stream;
 public class SimpleGrep {
     public static void main(String args[]) {
 
+        if(args.length != 2) {
+            System.out.println("Incorrect count of parameters. Please enter fileName and substring as parameters!");
+            System.exit(1);
+        }
+
         String fileName = args[0];
         String subString = args[1];
-        List<String> list = new ArrayList<>();
 
         if (Files.notExists(Paths.get(fileName))) {
             System.out.println("There is no file with name " + fileName);
         } else {
             try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
 
-                list = stream
+                stream
                         .filter(line -> line.contains(subString))
-                        .collect(Collectors.toList());
-                //I'm no write subString in Output, because i would like to save values in the list
+                        .forEach(System.out::println);
 
             } catch (IOException e) {
-                e.printStackTrace();
+                throw new RuntimeException("Unbelievable error while reading the file " + fileName +"\n", e);
             }
 
-            if (!list.isEmpty()) {
-                list.forEach(System.out::println);
-            } else {
-                System.out.println("There is no matches of " + subString + " in the file " + fileName);
-            }
 
         }
     }
